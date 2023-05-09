a97c4b5031d39fe48e6cf7ff6b1cd111a63a52e4
ViniciusArenhartKotz <vinicius-akotz@educar.rs.gov.br>
2023-05-09 20:14:36 -0300

    enviar

---
+++ src/campeonato/Posicao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package campeonato;
+
+/**
+ *
+ * @author Escola
+ */
+public enum Posicao {
+    
+    GOLEIRO,
+    ZAGUEIRO,
+    LATERAL,
+    VOLANTE,
+    MEIA,
+    ATACANTE
+            }
+            
